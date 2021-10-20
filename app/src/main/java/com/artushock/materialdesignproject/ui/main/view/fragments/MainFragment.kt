a42443f23042ba4ced7056636c5424b7af813d1e
Artiom Ponomariov <artushock@gmail.com>
2021-10-20 14:52:30 +0300

    Added rendering information in bottomsheet and handling states

--- app/src/main/java/com/artushock/materialdesignproject/ui/main/view/fragments/MainFragment.kt
+++ app/src/main/java/com/artushock/materialdesignproject/ui/main/view/fragments/MainFragment.kt
@@ -3,14 +3,15 @@ package com.artushock.materialdesignproject.ui.main.view.fragments
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import coil.load
-import com.artushock.materialdesignproject.R
 import com.artushock.materialdesignproject.data.model.PictureOfTheDayData
 import com.artushock.materialdesignproject.databinding.MainFragmentBinding
 import com.artushock.materialdesignproject.ui.main.viewmodel.MainViewModel
@@ -60,9 +61,43 @@ class MainFragment : Fragment() {
         }
     }
 
-    private fun setBottomSheetBehavior(bottomSheet: ConstraintLayout){
+    private fun setBottomSheetBehavior(bottomSheet: ConstraintLayout) {
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+
+        binding.includedBottomSheet.bottomSheetDescriptionHeader.text =
+            "Information is waited... "
+
+        bottomSheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+                        Toast.makeText(context, "STATE_COLLAPSED", Toast.LENGTH_SHORT).show()
+                    }
+                    BottomSheetBehavior.STATE_DRAGGING -> {
+                        Toast.makeText(context, "STATE_DRAGGING", Toast.LENGTH_SHORT).show()
+                    }
+                    BottomSheetBehavior.STATE_EXPANDED -> {
+                        Toast.makeText(context, "STATE_EXPANDED", Toast.LENGTH_SHORT).show()
+                    }
+                    BottomSheetBehavior.STATE_HALF_EXPANDED -> {
+                        Toast.makeText(context, "STATE_HALF_EXPANDED", Toast.LENGTH_SHORT).show()
+                    }
+                    BottomSheetBehavior.STATE_HIDDEN -> {
+                        Toast.makeText(context, "STATE_HIDDEN", Toast.LENGTH_SHORT).show()
+                    }
+                    BottomSheetBehavior.STATE_SETTLING -> {
+                        Toast.makeText(context, "STATE_SETTLING", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                Log.d("123123123", "slideOffset $slideOffset")
+            }
+        })
+
     }
 
     private fun initViewModel() {
@@ -73,17 +108,29 @@ class MainFragment : Fragment() {
         when (data) {
             is PictureOfTheDayData.Success -> {
                 val serverResponseData = data.serverResponseData
-//                binding.title.text = serverResponseData.title
-//                binding.description.text = serverResponseData.explanation
+                setPhotoDescriptionIntoBottomSheet(
+                    serverResponseData.title,
+                    serverResponseData.explanation
+                )
                 val url = serverResponseData.url
                 if (url.isNullOrEmpty()) {
-                    //Todo
+                    Toast.makeText(context, "No APOD url!", Toast.LENGTH_SHORT).show()
                 } else {
                     binding.imageView.load(url) {
                         lifecycle(this@MainFragment)
                     }
                 }
             }
+        }
+    }
+
+    private fun setPhotoDescriptionIntoBottomSheet(title: String?, description: String?) {
+        val t = if (title.isNullOrEmpty()) "No title" else title
+        val d = if (description.isNullOrEmpty()) "No description" else description
+
+        with(binding.includedBottomSheet) {
+            bottomSheetDescriptionHeader.text = t
+            bottomSheetDescription.text = d
         }
     }
 }
